5cb643e29bb55c90739347bdf83370ce31736f1f
TheBloodAlchemist <birdie570@gmail.com>
2015-03-19 10:25:50 -0400

    Update Matrix.java

--- src/usefulfunctions/math/algebra/Matrix.java
+++ src/usefulfunctions/math/algebra/Matrix.java
@@ -1,4 +1,19 @@
 package usefulfunctions.math.algebra;
 public class Matrix
 {
+  private double[][] data;
+  private int rows;
+  private int cols;
+  
+  public Matrix(int rows, int cols)
+  {
+    this.rows = rows;
+    this.cols = cols;
+    data = new double[rows][cols];
+  }
+  
+  public Matrix(double[][] data)
+  {
+    this.data = data;
+  }
 }
