edb6d4e290c7abd29e3840d3d8dcc260dc5c75c0
TheBloodAlchemist <birdie570@gmail.com>
2015-03-21 18:51:47 -0400

    Update Quadratic.java

--- src/usefulfunctions/math/algebra/Quadratic.java
+++ src/usefulfunctions/math/algebra/Quadratic.java
@@ -1,1 +1,36 @@
+package usefulfunctions.math.algebra;
 
+public class Quadratic 
+{
+	private double a, b, c;
+	
+	public Quadratic(double a, double b, double c)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double discriminant()
+	{
+		return ((b * b) - (4 * a * c));
+	}
+}
+package usefulfunctions.math.algebra;
+
+public class Quadratic 
+{
+	private double a, b, c;
+	
+	public Quadratic(double a, double b, double c)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double discriminant()
+	{
+		return ((b * b) - (4 * a * c));
+	}
+}
